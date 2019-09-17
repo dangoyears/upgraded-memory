721bfe8c291116e2d2142eb003b9d06c6ea95423
dangosky <1341514988@qq.com>
2019-09-17 23:23:41 +0800

    chore: 配置eslint
    Change-Id: I8f0c5492fff6c10878ef2f97f223fa3f2e02efe6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    'no-multi-spaces': 1,
+    'no-redeclare': 1, // disallow declaring the same variable more then once
+    'brace-style': 1, // enforce one true brace style
+    'camelcase': 1, // require camel case names
+    'new-cap': 1, // require a capital letter for constructors
+    'arrow-parens': ["error", "as-needed"], // 在箭头函数可以省略括号的地方强制不使用括号
+    'no-duplicate-imports': 2, // 禁止重复模块导入
+    'no-var': 2,
+    'prefer-arrow-callback': 2, // 要求回调函数使用箭头函数
+    'prefer-const': 2,
+    'prefer-rest-params': 1, // 要求使用剩余参数而不是 arguments
+  }
+}
